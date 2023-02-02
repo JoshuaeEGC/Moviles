b9198b205e9b4c6cbadb2218edee143add152c28
Is720948 <is720948@iteso.mx>
2023-02-01 19:51:05 -0600

    b

---
+++ tarea2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:app_iteso/pantalla.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App_ITESO',
+      home: pantalla(),
+    );
+  }
+}
